3cafc032e421a6774f8164450c7ccb3ce26b715d
kirandutt02-alt <kirandutt02@gmail.com>
2026-09-18 02:24:16 +0530

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,6 +1,7 @@
 def call(String channel, String message) {
     slackSend(
         channel: channel,
-        message: message
+        message: message,
+        tokenCredentialId: 'slack-token'
     )
 }
